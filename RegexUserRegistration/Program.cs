e6d85a9accdd6cd11c068d2acea930da9f3ea56f
yash-agrwl <agarwalyash7012@gmail.com>
2022-03-30 18:53:30 +0530

    added empty project file with welcome message

---
+++ RegexUserRegistration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RegexUserRegistration
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to the User Registration System Problem.\n");
+        }
+    }
+}
