dad39fa4f764846469bac3c27e2c5ea17d8b37b9
yash-agrwl <agarwalyash7012@gmail.com>
2022-05-12 15:33:32 +0530

    added comments for Custom Exception class

--- RegexUserRegistration/UserRegistrationCustomException.cs
+++ RegexUserRegistration/UserRegistrationCustomException.cs
@@ -8,15 +8,25 @@ namespace RegexUserRegistration
 {
     public class UserRegistrationCustomException : Exception
     {
-
+        /// <summary>
+        /// Enum for Custom Exception.
+        /// </summary>
         public enum ExceptionType
         {
             EMPTY_INPUT,
             INVALID_INPUT
         }
 
+        /// <summary>
+        /// Variable to indicate the type of Custom Exception.
+        /// </summary>
         public readonly ExceptionType Type;
 
+        /// <summary>
+        /// Parameterized Constructor sets the Exception Type and message.
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="message"></param>
         public UserRegistrationCustomException(ExceptionType type, string message) : base(message)
         {
             this.Type = type;
